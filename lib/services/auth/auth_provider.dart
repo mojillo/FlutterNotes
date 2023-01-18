fff354abe6a95f8164fa8dfc58d2c3931d3ff736
mojillo <parthsubs@gmail.com>
2023-01-18 19:03:21 +0530

    Added Auth layer

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,19 @@
+// This provides a universal auth provider for all the types of authentication services
+//we will provide a getter and setter for this purpose
+
+import 'package:mynotesapp/services/auth/auth_user.dart';
+
+abstract class Authprovider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> createUser({
+    // this enables a custom module for making a new user
+    required String email,
+    required String password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
